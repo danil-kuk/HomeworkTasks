cab6eb4e6c2e9a54bd81f94a59bee2a843fc7b85
danil-kuk <danilkuk5@gmail.com>
2019-09-03 21:31:17 +0500

    Добавлено решение 25 задачи

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<BigInteger>
+            {
+                1, 1
+            };
+            for (int i = 2; list[list.Count - 1].ToString().Length < 1000; i++)
+            {
+                list.Add(list[i - 1] + list[i - 2]);
+            }
+            Console.WriteLine(list.Count);
+        }
+    }
+}
