0ab6f489edab3b6f716a186584511dbbeb9ab66c
danil-kuk <danilkuk5@gmail.com>
2019-09-03 19:31:20 +0500

    Добавлено решение 18 задачи

---
+++ Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var triangle = File.ReadAllLines("Triangle.txt")
+                .Select(l => l.Split(' ')
+                .Select(s => int.Parse(s)).ToArray())
+                .Reverse().ToArray();
+            for (int i = 1; i < triangle.Length; i++)
+            {
+                for (int j = 0; j < triangle[i - 1].Length - 1; j++)
+                {
+                    triangle[i][j] += Math.Max(triangle[i - 1][j], triangle[i - 1][j + 1]);
+                }
+            }
+            Console.WriteLine(triangle.Last().Last());
+        }
+    }
+}
